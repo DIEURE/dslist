6be635327287959305b02b26bb63cbcda2e9e6c8
dieure <dieure@gmail.com>
2025-05-20 23:06:06 -0300

    Game Seed

--- src/main/java/com/autenticatelemetria/dslist/entities/Game.java
+++ src/main/java/com/autenticatelemetria/dslist/entities/Game.java
@@ -21,9 +21,14 @@ public class Game {
 	@Column(name="game_year")
 	private Integer year;
 	private String genre;
-	private String plataform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	//criar um construtor
@@ -31,17 +36,18 @@ public class Game {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String plataform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
-		
-		this.id               = id;
-		this.title            = title;
-		this.year             = year;
-		this.genre            = genre;
-		this.plataform        = plataform;
-		this.imgUrl           = imgUrl;
+		super();
+		this.id = id;
+		this.title = title;
+		this.year = year;
+		this.genre = genre;
+		this.platforms= platforms;
+		this.score = score;
+		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescription  = longDescription;
+		this.longDescription = longDescription;
 	}
 
 	public Long getId() {
@@ -76,12 +82,20 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlataform() {
-		return plataform;
+	public String getPlatform() {
+		return platforms;
 	}
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setPlatform(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -124,7 +138,8 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
-	 
+
+		 
 	 
 	
 	
