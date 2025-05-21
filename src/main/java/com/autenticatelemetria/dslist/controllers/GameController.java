8157edac7c47d08da6461c593e4b3fdc22d5506e
dieure <dieure@gmail.com>
2025-05-21 00:12:00 -0300

    Game repository,service,controller

---
+++ src/main/java/com/autenticatelemetria/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.autenticatelemetria.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.autenticatelemetria.dslist.dto.GameMinDTO;
+import com.autenticatelemetria.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value="/games")
+public class GameController {
+	
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll(){
+		List<GameMinDTO> result = gameService.findAll();
+		return result;
+	}
+
+}
